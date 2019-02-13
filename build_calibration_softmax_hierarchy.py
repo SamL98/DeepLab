60538f1137beadae3e55e24c963bdccaf9828a54
Sam Lerner <lerner98@gmail.com>
2019-02-12 20:02:07 -0500

    added softmax calibration script

---
+++ build_calibration_softmax_hierarchy.py
@@ -0,0 +1,65 @@
+import numpy as np
+from hdf5storage import loadmat, savemat
+from os.path import join
+import sys
+
+from util import *
+
+tree_fname = sys.argv[1]
+slices = read_slices(tree_fname)
+
+imset = 'val'
+if len(sys.argv) > 2:
+	imset = sys.argv[2]
+
+ds_path = 'D:/datasets/processed/voc2012'
+ds_info = loadmat(join(ds_path, 'dataset_info.mat'))
+#m = ds_info['num_'+imset]
+m = 350
+nc = 20
+
+logit_path = join(ds_path, 'deeplab_prediction', imset, imset+'_%06d_logits.mat')
+gt_path = join(ds_path, 'truth', imset, imset+'_%06d_pixeltruth.mat')	
+
+nb = len(slices[0][0].acc_hist)
+res = 1./nb
+
+for idx in range(1, m+1):
+	print('Binning logit no. %d' % idx)
+
+	logits = loadmat(logit_path % idx)['logits_img'][...,1:].reshape(-1, nc)
+	zero_vec = np.zeros((len(logits)), dtype=logits.dtype)[...,np.newaxis] # shape = (len(logits), 1)
+	logits = np.concatenate((zero_vec, logits), axis=1)
+
+	gt = loadmat(gt_path % idx)['truth_img'].ravel()
+
+	# discard pixels that were either background or void in ground truth 
+	fg_mask = (gt>0) & (gt<=nc)
+	logits = logits[fg_mask]
+	gt = gt[fg_mask]
+
+	exp_logits = np.exp(logits)
+	sm = exp_logits / np.maximum(np.sum(exp_logits, axis=-1)[...,np.newaxis], 1e-7)
+
+	for i, slc in enumerate(slices):
+		for j, cluster in enumerate(slc):
+			for true_label, sm_vec in zip(gt, sm):
+				label = remap_gt(true_label, slc)
+				if label != j: continue
+				
+				sm_vec = remap_logits(sm_vec, slc)
+				conf = sm_vec[j]
+				binno = np.floor(conf/res).astype(np.uint8)
+				binno = min(binno, nb-1)
+
+				if np.argmax(sm_vec) == label:
+					cluster.corr_hist[binno] += 1
+
+				cluster.count_hist[binno] += 1
+
+
+for slc in slices:
+	for cluster in slc:
+		cluster.acc_hist[:] = cluster.corr_hist.astype(np.float32) / cluster.count_hist.astype(np.float32)
+
+save_slices(tree_fname[:tree_fname.index('.')]+'_sm.pkl', slices)

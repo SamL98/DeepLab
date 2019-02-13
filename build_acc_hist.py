b47bccf7a421257a08af619bb470c261191ee504
Lerner <lerner.67a@coeit.osu.edu>
2019-02-13 15:38:49 +0000

    updated reliability diagram script

--- build_acc_hist.py
+++ build_acc_hist.py
@@ -10,31 +10,36 @@ PRED_PATH = join(DS_PATH, 'Deeplab_Prediction', imset)
 GT_PATH = join(DS_PATH, 'Truth', imset)
 
 ds_info = loadmat(join(DS_PATH, 'dataset_info.mat'))
-num_img = ds_info['num_'+imset.lower()]
+num_img = 350
+if imset.lower == 'test':
+	num_img = 1449-num_img
 
-SM_FMT = '%s_%06d_softmax.mat'
-SM_MAT_NAME = 'softmax_img'
+LGT_FMT = join(PRED_PATH, imset.lower()+'_%06d_logits.mat')
+LGT_MAT_NAME = 'logits_img'
 
-PRED_FMT = '%s_%06d_prediction.mat'
-PRED_MAT_NAME = 'pred_img'
-
-GT_FMT = '%s_%06d_pixeltruth.mat'
+GT_FMT = join(GT_PATH, imset.lower()+'_%06d_pixeltruth.mat')
 GT_MAT_NAME = 'truth_img'
-
-def load_sm_pred_gt(idx):
-	global PRED_PATH, SM_FMT, SM_MAT_NAME, GT_PATH, GT_FMT, GT_MAT_NAME, imset
-	sm = loadmat(join(PRED_PATH, SM_FMT % (imset, idx)))[SM_MAT_NAME].flatten()
-	pred = loadmat(join(PRED_PATH, PRED_FMT % (imset, idx)))[PRED_MAT_NAME].flatten()
-	gt = loadmat(join(GT_PATH, GT_FMT % (imset, idx)))[GT_MAT_NAME].flatten()
-	return sm, pred, gt
-	
 	
 acc_res = 0.1
 correct_hist = np.zeros((int(1./acc_res)), dtype=np.uint64)
 count_hist = np.zeros_like(correct_hist)
 	
-for i in range(1, num_img+1):
-	scores, pred, gt = load_sm_pred_gt(i)
+for idx in range(1, num_img+1):
+	gt = loadmat(GT_FMT % idx)[GT_MAT_NAME].ravel()
+	mask = (gt>0) & (gt<255)
+
+	logits = loadmat(LGT_FMT % idx)[LGT_MAT_NAME].reshape(-1, 21)
+	logits[:,0] = 0
+	
+	gt = gt[mask]
+	logits = logits[mask]
+	
+	pred = np.argmax(logits, axis=-1)
+	
+	exp_logits = np.exp(logits)
+	sm = exp_logits / np.maximum(1e-7, exp_logits.sum(-1)[:,np.newaxis])
+	scores = np.max(sm, axis=-1)
+	
 	bins = np.floor(scores/acc_res).astype(np.uint8)
 	
 	for binno in np.unique(bins):
@@ -45,17 +50,43 @@ for i in range(1, num_img+1):
 		
 
 acc_hist = correct_hist.astype(np.float64)/np.maximum(1e-7, count_hist)
+
+below_vals, above_vals = [], []
+colors = []
+
+for i, acc in enumerate(acc_hist):
+	val = i*acc_res
+	
+	if acc < val:
+		below_vals.append(acc)
+		above_vals.append(val-acc)
+		colors.append('r')
+	else:
+		below_vals.append(val)
+		above_vals.append(acc-val)
+		colors.append('tab:orange')
 		
 import matplotlib.pyplot as plt
+from matplotlib.patches import Patch
+
 fig, ax = plt.subplots()
-ax.bar(range(acc_hist.shape[0]), [v*acc_res for v in range(acc_hist.shape[0])], align='edge', color='r', alpha=0.5)
-ax.bar(range(acc_hist.shape[0]), acc_hist, align='edge')
+
+ax.bar(range(len(below_vals)), below_vals, align='edge')
+ax.bar(range(len(above_vals)), above_vals, align='edge', bottom=below_vals, color=colors)
+
 ax.set_xlabel('Confidence')
 ax.xaxis.set_ticks(np.linspace(0, acc_hist.shape[0], num=6))
 ax.set_xticklabels(['%.1f' % v for v in np.linspace(0.0, 1.0, num=6)])
+
 ax.set_ylabel('Accuracy')
 ax.yaxis.set_ticks(np.linspace(0.0, 1.0, num=6))
-#ax.plot(np.linspace(0, acc_hist.shape[0], num=6), np.linspace(0.0, 1.0, num=6))
-ax.set_title('%s Set' % imset)
-fig.savefig('acc_hist.png', bbox_inches='tight')
+
+ax.set_title('Accuracy on %s Set' % imset)
+
+blue_patch = Patch(color='b', label='Correct Accuracy')
+red_patch = Patch(color='r', label='Amount Overconfident')
+orange_patch = Patch(color='tab:orange', label='Amount Underconfident')
+ax.legend(handles=[blue_patch, red_patch, orange_patch])
+
+fig.savefig('images/acc_hist.png', bbox_inches='tight')
 plt.show()
--- build_conf_hist.py
+++ build_conf_hist.py
@@ -6,34 +6,40 @@ import sys
 imset = sys.argv[1].lower().capitalize()
 
 DS_PATH = 'D:/datasets/processed/voc2012' 
+GT_PATH = join(DS_PATH, 'Truth', imset)
 PRED_PATH = join(DS_PATH, 'Deeplab_Prediction', imset)
 
 ds_info = loadmat(join(DS_PATH, 'dataset_info.mat'))
-num_img = ds_info['num_'+imset.lower()]
+num_img = 350
 
-SM_FMT = '%s_%06d_softmax.mat'
-SM_MAT_NAME = 'softmax_img'
+if imset.lower() != 'val':
+	num_img = 1449-num_img
 
-def load_softmax(idx):
-	global PRED_PATH, SM_FMT, SM_MAT_NAME, imset
-	return loadmat(join(PRED_PATH, SM_FMT % (imset, idx)))[SM_MAT_NAME][:]
-	
+LGT_FMT = join(PRED_PATH, imset.lower()+'_%06d_logits.mat')
+LGT_MAT_NAME = 'logits_img'
+
+GT_FMT = join(GT_PATH, imset.lower()+'_%06d_pixeltruth.mat')
+GT_MAT_NAME = 'truth_img'
 	
 conf_res = 0.05
 conf_hist = np.zeros((int(1./conf_res)), dtype=np.uint64)
-#count_hist = np.zeros_like(conf_hist)
 	
-for i in range(1, num_img+1):
-	scores = load_softmax(i).flatten()
+for idx in range(1, num_img+1):
+	gt = loadmat(GT_FMT % idx)[GT_MAT_NAME].ravel()
+	mask = (gt>0) & (gt<255)
+
+	logits = loadmat(LGT_FMT % idx)[LGT_MAT_NAME][...,1:].reshape(-1, 20)
+	logits = logits[mask]
+	
+	exp_logits = np.exp(logits)
+	sm = exp_logits / np.maximum(1e-7, exp_logits.sum(-1)[:,np.newaxis])
+	scores = np.max(sm, axis=-1)
+	
 	bins = np.floor(scores/conf_res).astype(np.uint8)
 	
 	for binno in np.unique(bins):
 		conf_hist[min(binno, conf_hist.shape[0]-1)] += np.sum(bins==binno)
-		#conf_hist[min(binno, conf_hist.shape[0]-1)] += np.sum(scores[bins==binno])
-		#count_hist[min(binno, count_hist.shape[0]-1)] += np.sum(bins==binno)
 
-		
-#conf_hist = conf_hist.astype(np.float64)/np.maximum(1e-7, count_hist.astype(np.float64))
 conf_hist = conf_hist.astype(np.float64)/conf_hist.sum()
 		
 import matplotlib.pyplot as plt
@@ -44,6 +50,6 @@ ax.xaxis.set_ticks(np.linspace(0, conf_hist.shape[0], num=6))
 ax.set_xticklabels(['%.1f' % v for v in np.linspace(0.0, 1.0, num=6)])
 ax.set_ylabel('% of pixels')
 ax.yaxis.set_ticks(np.linspace(0.0, 1.0, num=6))
-ax.set_title('%s Set' % imset)
-fig.savefig('conf_hist.png', bbox_inches='tight')
+ax.set_title('Confidence on %s Set' % imset)
+fig.savefig('images/conf_hist.png', bbox_inches='tight')
 plt.show()

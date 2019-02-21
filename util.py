327d597eda4a7e17780b98c4c4d97e151693a48e
Sam Lerner <lerner98@gmail.com>
2019-02-21 11:22:26 -0500

    updated hierarchy util functions

--- util.py
+++ util.py
@@ -175,7 +175,7 @@ Tree Utilities
 '''
 
 def get_depth_of_label(pred_label, slices):
-	if pred_label < len(slices[0]):
+	if pred_label <= len(slices[0]):
 		# If pred_label is a terminal, it's parent could be a few levels up in the hierarchy.
 		#
 		# Therefore, iterate through the slices until the terminal label is in a node with more than one child.
@@ -189,22 +189,21 @@ def get_depth_of_label(pred_label, slices):
 		# Otherwise, iterate through the slices until the predicted label is within the current slice and return that depth.
 		total_nodes = 0
 		for i, slc in enumerate(slices):
-			if pred_label > len(slc)+total_nodes+1:
-				total_nodes += len(slc)
-				continue
-			return len(slices)-i			
+			if pred_label <= len(slc) + total_nodes:
+				return len(slices)-i
+
+			total_nodes += len(slc)			
 
 def is_in_gt_path(pred_label, gt_label, slices):
 	total_nodes = 0
 	for slc in slices:
 		# Accumulate the total nodes before the current slice so that when gt_label is remapped
 		# to the local indices of the slice, that base is added to test for equality with the predicted label.
-		if pred_label > len(slc)+total_nodes+1:
-			total_nodes += len(slc)
-			continue
-
-		gt_remapped = remap_gt(gt_label, slc) + total_nodes
-		return gt_remapped == pred_label
+		if pred_label <= len(slc) + total_nodes:
+			gt_remapped = remap_gt(gt_label, slc) + total_nodes
+			return gt_remapped == pred_label
+		
+		total_nodes += len(slc)
 
 
 

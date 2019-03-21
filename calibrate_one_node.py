d231565874d88d363e262a36144b881004b1c6b4
Lerner <lerner.67a@coeit.osu.edu>
2019-03-21 16:50:50 +0100

    fix

--- calibrate_one_node.py
+++ calibrate_one_node.py
@@ -1,8 +1,18 @@
 from util import *
 
-slices = read_slices('slices.pkl')
+slices = read_slices('slices-copy.pkl')
 
-for idx in range(1, num_img_for('val')):
+label = 1
+node_idx = 0
+
+slc = slices[0]
+node = slices[0][node_idx]
+
+airplane_pix_sum = 0
+
+for idx in range(1, num_img_for('val')+1):
+	stdout_writeln(str(idx))
+
 	logits = load_logits('val', idx, reshape=True)
 	gt = load_gt('val', idx, reshape=True)
 
@@ -10,19 +20,29 @@ for idx in range(1, num_img_for('val')):
 	logits = logits[fg_mask]
 	gt = gt[fg_mask]
 
+	if (gt == label).sum() == 0:
+		continue
+
+	sm = sm_of_logits(logits, start_idx=1, zero_pad=True)
+
+	airplane_pix_sum += (gt == label).sum()
+
 	terminal_pred = np.argmax(logits[:,1:], axis=-1) + 1
 
-	for i, slc in enumerate(slices):
-		slc_gt = np.array([remap_label(lab, slc) for lab in gt])
-		slc_term_pred = np.array([remap_label(pred, slc) for pred in terminal_pred])
-		slc_sm = np.array([remap_scores(sm_vec, slc) for sm_vec in sm])
+	slc_gt = np.array([remap_label(lab, slc) for lab in gt])
+	slc_term_pred = np.array([remap_label(pred, slc) for pred in terminal_pred])
+	slc_sm = np.array([remap_scores(sm_vec, slc) for sm_vec in sm])
 
-		node_idx = 1
-		pred_mask = slc_term_pred == node_idx
+	pred_mask = slc_term_pred == node_idx
 
-		slc_gt_masked = slc_gt[pred_mask]
-		slc_sm_masked = slc_sm[pred_mask]
+	slc_gt_masked = slc_gt[pred_mask]
+	slc_sm_masked = slc_sm[pred_mask]
 
-		sm_conf = slc_sm_masked[:,node_idx]
+	sm_conf = slc_sm_masked[:,node_idx]
 
-		node.accum_scores(sm_conf, slc_gt_masked == node_idx, 100, 0.1)
+	node.accum_scores(sm_conf, slc_gt_masked == node_idx, 100, 0.1)
+
+
+node_pix_sum = node.tot_hist.sum()
+
+print(airplane_pix_sum, node_pix_sum)

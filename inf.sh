adbc48f79236a4024ede044ed5f783a6fe3118b8
Sam Lerner <lerner98@gmail.com>
2019-03-28 13:59:47 -0400

    Added env var ds path check to inf script

--- inf.sh
+++ inf.sh
@@ -2,5 +2,10 @@ name="${1}"
 data_dir="calib_data/${name}"
 slices_name="calib_data/${name}/slices.pkl"
 
-mkdir "D:/datasets/processed/voc2012/deeplab_prediction/test/${name}"
+ds_path="D:/datasets/processed/voc2012"
+if [[ -z $DS_PATH ]]; then
+	ds_path=$DS_PATH
+fi
+
+mkdir "${ds_path}/deeplab_prediction/test/${name}"
 python do_inf.py --name=$name --slice_file=$slices_name 
